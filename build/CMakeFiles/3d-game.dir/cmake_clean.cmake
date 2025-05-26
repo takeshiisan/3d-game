086f85296bccba42598d6807a54b2dd10a236530
takeshiisan <takeshisan29@gmail.com>
2025-05-26 10:58:09 +0800

    first commit

---
+++ build/CMakeFiles/3d-game.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/3d-game.dir/link.d"
+  "3d-game"
+  "3d-game.pdb"
+  "CMakeFiles/3d-game.dir/src/3dgame.c.o"
+  "CMakeFiles/3d-game.dir/src/3dgame.c.o.d"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/3d-game.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
